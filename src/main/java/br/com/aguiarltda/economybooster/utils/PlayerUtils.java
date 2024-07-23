72e6403dc4137a9a50b433a4436a5f2a8694f103
FELIPE_AGUIAR <felipeferreiraaguiar9@gmail.com>
2024-07-23 19:51:46 -0400

    feat: Optimizing user search

--- src/main/java/br/com/aguiarltda/economybooster/utils/PlayerUtils.java
+++ src/main/java/br/com/aguiarltda/economybooster/utils/PlayerUtils.java
@@ -3,19 +3,13 @@ package br.com.aguiarltda.economybooster.utils;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-import java.util.Collection;
 import java.util.UUID;
 
 public class PlayerUtils {
   public static Player getPlayerByUniqueId(UUID uniqueId) {
-    Collection<? extends Player> onlinePlayers = Bukkit.getServer().getOnlinePlayers();
-
-    for (Player player : onlinePlayers) {
-      if (player.getUniqueId().equals(uniqueId)) {
-        return player;
-      }
-    }
-
-    throw new IllegalArgumentException("[EconomyBooster] No online player was found with this identifier");
+    return Bukkit.getServer().getOnlinePlayers().stream()
+            .filter((player) -> player.getUniqueId().equals(uniqueId))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("[EconomyBooster] No online player was found with this identifier"));
   }
 }
